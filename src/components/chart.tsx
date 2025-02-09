032fc2244e8177ae104f5ef073be7df789a587d8
ricardobizerra <ricardo.bizerra19@gmail.com>
2025-02-09 20:00:03 -0300

    feat: enhance chart component to support pie charts and customizable tooltips

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -14,15 +14,19 @@ import {
   Bar,
   BarChart,
   CartesianGrid,
+  LabelList,
   Line,
   LineChart,
+  Pie,
+  PieChart,
   XAxis,
 } from 'recharts';
 
 const chartTypes = {
-  bar: { chart: BarChart, element: Bar },
-  line: { chart: LineChart, element: Line },
-  area: { chart: AreaChart, element: Area },
+  bar: BarChart,
+  line: LineChart,
+  area: AreaChart,
+  pie: PieChart,
 };
 
 type ChartType = keyof typeof chartTypes;
@@ -31,37 +35,90 @@ interface ChartProps<TData> {
   data: TData[];
   config: ChartConfig;
   type: ChartType;
+  tooltip?: {
+    indicator: 'line' | 'dot' | 'dashed';
+  };
 }
 
-export function Chart<TData>({ data, config, type }: ChartProps<TData>) {
-  const SelectedChart = chartTypes[type].chart;
-  const SelectedElement = chartTypes[type].element;
+export function Chart<TData>({
+  data,
+  config,
+  type,
+  tooltip = { indicator: 'line' },
+}: ChartProps<TData>) {
+  const SelectedChart = chartTypes[type];
 
   return (
     <ChartContainer config={config} className="min-h-[200px] w-full">
       <SelectedChart accessibilityLayer data={data}>
         <CartesianGrid vertical={false} />
-        <XAxis
-          dataKey="month"
-          tickLine={false}
-          tickMargin={10}
-          axisLine={false}
-          tickFormatter={(value) => value.slice(0, 3)}
-        />
-        <ChartTooltip content={<ChartTooltipContent indicator="line" />} />
-        <ChartLegend content={<ChartLegendContent />} />
-        <SelectedElement
-          dataKey="desktop"
-          fill="var(--color-desktop)"
-          stroke="var(--color-desktop)"
-          radius={4}
-        />
-        <SelectedElement
-          dataKey="mobile"
-          fill="var(--color-mobile)"
-          stroke="var(--color-mobile)"
-          radius={4}
-        />
+        {type !== 'pie' && (
+          <XAxis
+            dataKey="month"
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+            tickFormatter={(value) => value.slice(0, 3)}
+          />
+        )}
+        {!!tooltip && (
+          <ChartTooltip
+            content={<ChartTooltipContent indicator={tooltip.indicator} />}
+          />
+        )}
+        {/* <ChartLegend content={<ChartLegendContent />} /> */}
+
+        {type === 'bar' &&
+          Object.keys(config).map((key) => (
+            <Bar
+              key={key}
+              dataKey={key}
+              fill={`var(--color-${key})`}
+              radius={4}
+            />
+          ))}
+
+        {type === 'line' &&
+          Object.keys(config).map((key) => (
+            <Line
+              key={key}
+              dataKey={key}
+              stroke={`var(--color-${key})`}
+              strokeWidth={2}
+            />
+          ))}
+
+        {type === 'area' &&
+          Object.keys(config).map((key) => (
+            <Area
+              key={key}
+              dataKey={key}
+              fill={`var(--color-${key})`}
+              stroke={`var(--color-${key})`}
+              strokeWidth={2}
+            />
+          ))}
+
+        {type === 'pie' &&
+          Object.keys(config).map((key) => (
+            <Pie
+              dataKey={key}
+              data={data.map((value, index) => ({
+                ...value,
+                fill: `hsl(var(--chart-${(index % 3) + 1}))`,
+              }))}
+              nameKey="month"
+              key={key}
+            >
+              <LabelList
+                dataKey="month"
+                className="fill-background"
+                stroke="none"
+                fontSize={12}
+                formatter={(value: keyof typeof config) => value}
+              />
+            </Pie>
+          ))}
       </SelectedChart>
     </ChartContainer>
   );
