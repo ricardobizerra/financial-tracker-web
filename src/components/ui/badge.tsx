cea3f5b93a1de7fb46e36901475000e46279338d
ricardobizerra <ricardo.bizerra19@gmail.com>
2025-06-23 21:36:09 -0300

    chore: add size variants on badge

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const badgeVariants = cva(
-  'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  'inline-flex items-center rounded-md border text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
     variants: {
       variant: {
@@ -18,9 +18,16 @@ const badgeVariants = cva(
         success:
           'border-transparent bg-green-700 text-white shadow hover:bg-green-700/80',
       },
+      size: {
+        default: 'px-2.5 py-0.5 gap-1',
+        xs: 'px-1 gap-0.5',
+        sm: 'px-1.5 py-0.5 gap-0.5',
+        lg: 'px-4 py-1 text-sm gap-1.5',
+      },
     },
     defaultVariants: {
       variant: 'default',
+      size: 'default',
     },
   },
 );
@@ -29,9 +36,12 @@ export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof badgeVariants> {}
 
-function Badge({ className, variant, ...props }: BadgeProps) {
+function Badge({ className, variant, size, ...props }: BadgeProps) {
   return (
-    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+    <div
+      className={cn(badgeVariants({ variant, size }), className)}
+      {...props}
+    />
   );
 }
 
