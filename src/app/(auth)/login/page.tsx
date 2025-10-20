3150fbcdcf5c3df281cd3fba3bf347afd2640a59
ricardobizerra <ricardo.bizerra19@gmail.com>
2025-10-20 12:00:23 -0300

    fix: auth layout

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,5 @@
 import { AuthSignInForm } from '@/modules/auth/components/auth-sign-in-form';
 
 export default function Page() {
-  return (
-    <section className="max-w-screen flex h-full w-screen flex-col bg-muted px-6 py-4">
-      <AuthSignInForm />
-    </section>
-  );
+  return <AuthSignInForm />;
 }
